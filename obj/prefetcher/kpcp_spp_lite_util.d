ab2f6c3b71839142b7fde99ab20f79294810dd6d
mariahchongzi <1178401713@qq.com>
2017-11-13 17:22:19 +0800

    add files

---
+++ obj/prefetcher/kpcp_spp_lite_util.d
@@ -0,0 +1,1 @@
+obj/prefetcher/kpcp_spp_lite_util.o: prefetcher/kpcp_spp_lite_util.cc
---
+++ run_champsim.sh
@@ -0,0 +1,10 @@
+#TRACE_DIR=/your/trace/directory/
+TRACE_DIR=/Volumes/HFS+/project/champsim/ChampSim-master/CRC2_trace
+binary=${1}
+n_warm=${2}
+n_sim=${3}
+trace=${4}
+option=${5}
+
+mkdir -p results_${n_sim}M
+(./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces ${TRACE_DIR}/${trace}.trace.gz) &> results_${n_sim}M/${trace}-${binary}${option}.txt
